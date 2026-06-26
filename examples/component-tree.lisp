522895117d45d911fc08e82117d0fc89eadabea7
adolenc <mail@adolenc.me>
2026-06-26 23:36:39 +0200

    Add an example with drawing component tree

---
+++ examples/component-tree.lisp
@@ -0,0 +1,69 @@
+(ql:quickload :opendaq :silent t)
+
+(defparameter *instance* (make-instance 'daq:instance))
+(daq:add-device *instance* "daqref://device0")
+
+(defun type-label (component)
+  "Readable type name for COMPONENT, e.g. \"Channel\" or \"FunctionBlock\"."
+  (remove #\- (string-capitalize
+               (symbol-name (or (daq:component-type component) 'component)))))
+
+(defun type-name (value-type)
+  "Readable name for a property's core type, e.g. :DAQ-CT-INT => \"Int\"."
+  (let ((s (symbol-name value-type)))
+    (remove #\- (string-capitalize
+                 (if (eql 0 (search "DAQ-CT-" s)) (subseq s 7) s)))))
+
+(defparameter *boxed-types*
+  '((:daq-ct-bool   . daq:daq-boolean)
+    (:daq-ct-int    . daq:daq-integer)
+    (:daq-ct-float  . daq:daq-float)
+    (:daq-ct-string . daq:daq-string-object)
+    (:daq-ct-ratio  . daq:daq-ratio)
+    (:daq-ct-complex-number . daq:complex-number))
+  "Maps a property's core type to the boxed-primitive class VALUE-OF reads it as.
+Types absent here (lists, dicts, structs, nested objects, ...) are not scalars.")
+
+(defun property-value-string (property-object property)
+  "Printable value of PROPERTY on PROPERTY-OBJECT.  Scalar values are unboxed to
+their native Lisp value; structured ones are shown as \"<Type>\"."
+  (let ((boxed (cdr (assoc (daq:value-type property) *boxed-types*))))
+    (if boxed
+        (format nil "~S" (daq:value-of (daq:property-value property-object
+                                                           (daq:name property))
+                                       boxed))
+        (format nil "<~A>" (type-name (daq:value-type property))))))
+
+(defun draw-properties (component prefix)
+  "Print the visible properties of COMPONENT, each line indented with PREFIX."
+  (when (daq:supports-interface-p component 'daq:property-object)
+    (let ((object (daq:as component 'daq:property-object)))
+      (dolist (property (daq:visible-properties object))
+        (format t "~A• ~A : ~A = ~A~%"
+                prefix (daq:name property)
+                (type-name (daq:value-type property))
+                (property-value-string object property))))))
+
+(defun children (component)
+  "The immediate child components of COMPONENT if it is a folder, else NIL."
+  (when (daq:supports-interface-p component 'daq:folder)
+    (daq:items (daq:as component 'daq:folder) nil)))
+
+(defun draw-children (component prefix)
+  (let* ((kids (children component))
+         (n (length kids)))
+    (loop for child in kids
+          for i from 1
+          for last = (= i n)
+          for child-prefix = (concatenate 'string prefix (if last "   " "│  "))
+          do (format t "~A~A~A : ~A (~A)~%"
+                     prefix (if last "└─ " "├─ ")
+                     (daq:name child) (type-label child) (daq:local-id child))
+             (draw-properties child child-prefix)
+             (draw-children child child-prefix))))
+
+(let ((root (daq:root-device *instance*)))
+  (format t "~A : ~A (~A)~%"
+          (daq:name root) (type-label root) (daq:local-id root))
+  (draw-properties root "")
+  (draw-children root ""))
--- examples/streamreader.lisp
+++ examples/streamreader.lisp
@@ -6,4 +6,6 @@
 (let* ((channel (daq:as (daq:find-component *instance* "Dev/RefDev0/IO/AI/RefCh0") 'daq:channel))
        (signal (first (daq:signals (daq:as channel 'daq:channel))))
        (reader (make-instance 'daq:stream-reader :signal signal)))
-  (format t "~A~%" (daq:read reader 100 :timeout-ms 1000)))
+  (format t "some samples: ~A~%" (daq:read reader 100 :timeout-ms 1000))
+  (format t "and more samples: ~A~%" (daq:read reader 100 :timeout-ms 1000))
+  (format t "and more still: ~A~%" (daq:read reader 100 :timeout-ms 1000)))
